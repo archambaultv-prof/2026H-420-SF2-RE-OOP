08a1048ec84fa944ccaf94dddb437cfbd9964aab
Hakim Ihadadende <e6376143@cmaisonneuve.qc.ca>
2026-02-25 10:06:53 -0500

    Version 2 du zoo

--- animal.py
+++ animal.py
@@ -3,35 +3,51 @@ Module animal - Gestion procédurale des animaux du refuge
 Représentation sous forme de tuple: (nom, espèce, âge, santé)
 """
 
-# Indices du tuple animal
-NOM = 0
-ESPECE = 1
-AGE = 2
-SANTE = 3
+from abc import ABC, abstractmethod
 
-ESPECES = ["Tigre", "Singe", "Pingouin", "Autruche"]
+class Animal(ABC):
 
+    def __init__(self, nom, espece, age, sante):
+        self.nom = nom
+        self.espece = espece
+        self.age = age
+        self.sante = sante
 
-def creer_animal(nom: str, espece: str, age: int, sante: int = 100) -> tuple:
-    """Crée un animal: (nom, espèce, âge, santé)"""
-    if espece not in ESPECES:
-        raise ValueError(f"Espèce invalide. Choisir parmi: {ESPECES}")
-    if not 0 <= sante <= 100:
-        raise ValueError("Santé doit être entre 0 et 100")
-    return (nom, espece, age, sante)
+    @abstractmethod
+    def faire_bruit(self):
+        pass
 
-
-def afficher_animal(animal: tuple) -> str:
-    """Affiche l'animal de manière lisible."""
-    return f"🦁 [{animal[ESPECE]}] {animal[NOM]} ({animal[AGE]}ans, santé: {animal[SANTE]}%)"
+    def afficher_animal(self):
+        print(f" [{self.espece}] {self.nom} ({self.age}ans, santé: {self.sante}%)")
 
 
-def animal_faire_bruit(animal: tuple) -> str:
-    """Retourne le bruit selon l'espèce (polymorphisme)."""
-    bruits = {
-        "Tigre": "🐅 RAAAAAHHH!",
-        "Singe": "🐵 Ouh ouh ouh!",
-        "Pingouin": "🐧 Coin coin!",
-        "Autruche": "🦤 Hou hou!"
-    }
-    return bruits.get(animal[ESPECE], "...")
+class Tigre(Animal):
+    def __init__(self, nom, age, sante):
+        super().__init__(nom,'Tigre', age, sante)
+
+    def faire_bruit(self):
+        print("🐅 RAAAAAHHH!")
+
+
+class Singe(Animal):
+    def __init__(self, nom, age, sante):
+        super().__init__(nom, 'Singe', age, sante)
+
+    def faire_bruit(self):
+        print("🐵 Ouh ouh ouh!")
+
+
+class Pingouin(Animal):
+    def __init__(self, nom, age, sante):
+        super().__init__(nom, 'Pingouin', age, sante)
+
+    def faire_bruit(self):
+        print("🐧 Coin coin!")
+
+
+class Autruche(Animal):
+    def __init__(self, nom, age, sante):
+        super().__init__(nom, 'Autruche', age, sante)
+
+    def faire_bruit(self):
+        print("🦤 Hou hou!")
